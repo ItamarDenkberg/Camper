2d8814fe6071f8995a6a2e00c4f07f6bd715b637
Itamar Denkberg <ItamarDenkberg@users.noreply.github.com>
2021-08-22 22:02:28 +0300

    Squirrel Spawn Egg

--- src/main/java/io/github/itamardenkberg/camper/Camper.java
+++ src/main/java/io/github/itamardenkberg/camper/Camper.java
@@ -4,14 +4,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.github.itamardenkberg.camper.common.entities.passive.SquirrelEntity;
+import io.github.itamardenkberg.camper.common.items.CustomSpawnEggItem;
 import io.github.itamardenkberg.camper.core.init.BlockInit;
 import io.github.itamardenkberg.camper.core.init.EntityTypesInit;
 import io.github.itamardenkberg.camper.core.init.ItemInit;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -31,6 +35,11 @@ public class Camper {
 		BlockInit.BLOCKS.register(bus);
 
 		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public static void onRegisterEntities(final RegistryEvent.Register<EntityType<?>> event) {
+		CustomSpawnEggItem.initSpawnEggs();
 	}
 
 	@SuppressWarnings("deprecation")
--- src/main/java/io/github/itamardenkberg/camper/core/init/ItemInit.java
+++ src/main/java/io/github/itamardenkberg/camper/core/init/ItemInit.java
@@ -1,6 +1,7 @@
 package io.github.itamardenkberg.camper.core.init;
 
 import io.github.itamardenkberg.camper.Camper;
+import io.github.itamardenkberg.camper.common.items.CustomSpawnEggItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -13,7 +14,10 @@ public class ItemInit {
 	public static final RegistryObject<Item> HAZELNUT = ITEMS.register("hazelnut",
 			() -> new Item(new Item.Properties().food(FoodInit.HAZELNUT).group(Camper.TAB_CAMPER)));
 
+	public static final RegistryObject<CustomSpawnEggItem> SQUIRREL_SPAWN_EGG = ITEMS.register("squirrel_spawn_egg",
+			() -> new CustomSpawnEggItem(EntityTypesInit.SQUIRREL, 0x714622, 0xdcc4ab,
+					new Item.Properties().food(FoodInit.HAZELNUT).group(Camper.TAB_CAMPER)));
+
 	public static final RegistryObject<BlockItem> HAZEL_LEAVES = ITEMS.register("hazel_leaves",
-			() -> new BlockItem(BlockInit.HAZEL_LEAVES.get(),
-					new Item.Properties().maxStackSize(16).group(Camper.TAB_CAMPER)));
+			() -> new BlockItem(BlockInit.HAZEL_LEAVES.get(), new Item.Properties().group(Camper.TAB_CAMPER)));
 }
